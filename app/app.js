71bef214e41f1343fb4d4bc72775a3b0dca01cb6
root <root@ubuntu-2gb-blr1-01.localdomain>
2017-09-13 21:01:01 +0000

    setup

---
+++ app/app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('growSure', ['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/views/dashboard.html"
+    })
+     .when("/about-us", {
+        templateUrl: "app/views/about.html"
+     })
+         .when("/contact-us", {
+             templateUrl: "app/views/contact.html"
+         })
+    .otherwise({
+        redirect: '/'
+    });;
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.post = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};
+});
